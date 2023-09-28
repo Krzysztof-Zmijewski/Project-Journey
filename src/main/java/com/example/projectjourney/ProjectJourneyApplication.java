cab8832ae06692d05da0cb518e9b35817fada194
Krzysztof-Zmijewski <krzysztof.zmijewski00@wp.pl>
2023-09-28 18:01:16 +0200

    Initial commit.

---
+++ src/main/java/com/example/projectjourney/ProjectJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJourneyApplication.class, args);
+    }
+
+}
