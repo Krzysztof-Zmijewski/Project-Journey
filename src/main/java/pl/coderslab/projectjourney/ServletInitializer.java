6e5125e52a6c4d9f81873c1bdb5745e7b6f9b7e7
Krzysztof-Zmijewski <krzysztof.zmijewski00@wp.pl>
2023-10-02 14:53:17 +0200

    Directory name fix.

--- src/main/java/pl/coderslab/projectjourney/ServletInitializer.java
+++ src/main/java/pl/coderslab/projectjourney/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.projectjourney;
+package pl.coderslab.projectjourney;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
